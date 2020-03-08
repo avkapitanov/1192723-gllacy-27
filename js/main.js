51df15775da9ff4fad9c75a2937a43045a70d235
Artem Kapitanov <avkapitanov@yandex.ru>
2020-03-08 15:30:34 +0300

    Fix some errors Add overlay to modal Minify css and js

--- js/main.js
+++ js/main.js
@@ -57,15 +57,47 @@ document.addEventListener("DOMContentLoaded", function(){
 
   var formLink = document.querySelector(".btn-form");
   var popup = document.querySelector(".modal-feedback");
+  var overlay = document.querySelector(".modal-overlay");
   var formClose = document.querySelector(".feedback-form-close");
+  var form = popup.querySelector(".feedback-form");
+
+  var name = popup.querySelector("[name=name]");
+  var email = popup.querySelector("[name=email]");
+  var text = popup.querySelector("[name=message]");
 
   formLink.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("modal-show");
   });
 
+  overlay.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+  });
+
   formClose.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+  });
+
+  form.addEventListener("submit", function (evt) {
+    if (!name.value || !email.value || !text.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    }
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+      }
+    }
   });
 });
